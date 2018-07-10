ae524f41ed44c43aee88221dc38cbd655bc7cdda
noah-berman <noahberm@gmail.com>
2018-07-10 15:50:43 -0400

    added new and create for playerteam controller

--- app/controllers/player_teams_controller.rb
+++ app/controllers/player_teams_controller.rb
@@ -1,11 +1,23 @@
 class PlayerTeamsController < ApplicationController
 
   def new
-    #code
+    @player = Player.find(params[:id])
+    @playerteam = PlayerTeam.new
   end
 
   def create
-    #code
+    @playerteam = PlayerTeam.new(player_team_params)
+    if @playerteam.save
+      redirect_to team_path(@playerteam.team_id)
+    else
+      render :new
+    end
   end
 
+  private
+
+
+  def player_team_params
+    params.require(:player_team).permit(:player_id, :team_id)
+  end
 end
